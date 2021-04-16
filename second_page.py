8b02040fbdc6e1a89f385ed6b92456e45e3e7c44
Dana Al Shekerchi <dana.562002@gmail.com>
2021-04-16 13:58:45 -0400

    Added messageboxes for errors

--- second_page.py
+++ second_page.py
@@ -15,6 +15,9 @@ class ingredients(QDialog, QWidget):
     def __init__(self):
         super().__init__()
 
+
+
+
         self.ingredient = QLabel("Ingredients", self)
         self.ingredient.move(30, 50)
         self.max_add_label = QLabel("Max number of ingredients ", self)
@@ -25,6 +28,7 @@ class ingredients(QDialog, QWidget):
         self.all_label.move(300, 25)
 
         self.time = QSpinBox(self)
+        self.time.setRange(0, 50000)
 
         self.ing1 = QLineEdit(self)
         self.ing2 = QLineEdit(self)
@@ -37,12 +41,15 @@ class ingredients(QDialog, QWidget):
         self.ing9 = QLineEdit(self)
         self.ing10 = QLineEdit(self)
 
+        self.ing = [self.ing1, self.ing2, self.ing3, self.ing4, self.ing5, self.ing6, self.ing7,
+                    self.ing8, self.ing9, self.ing10]
+
         self.list = QListWidget()
         data = data_reading.read_recipes(data_reading.RECIPES_FILE)
         data_reading.clean_ingredients(data)
-        clean = list(data_reading.get_ingredients(data))
-        for i in range(len(clean)):
-            self.list.insertItem(i, clean[i])
+        self.clean = list(data_reading.get_ingredients(data))
+        for i in range(len(self.clean)):
+            self.list.insertItem(i, self.clean[i])
         # self.list.setResizeMode(QListView_ResizeMode=)
 
         self.title = "Page 2"
@@ -55,16 +62,17 @@ class ingredients(QDialog, QWidget):
 
         self.line_edit = None
 
+
     def InitWindow(self):
         # self.setWindowIcon(QtGui.QIcon(self.icon))
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowTitle(self.title)
 
-        data = data_reading.read_recipes(data_reading.RECIPES_FILE)
-        data_reading.clean_ingredients(data)
-        clean = list(data_reading.get_ingredients(data))
+        # data = data_reading.read_recipes(data_reading.RECIPES_FILE)
+        # data_reading.clean_ingredients(data)
+        # clean = list(data_reading.get_ingredients(data))
         testing = ['check', 'one', 'two', 'three', 'white pepper', 'blue pepper']
-        completer = QCompleter(clean)
+        completer = QCompleter(self.clean)
         completer.setFilterMode(Qt.MatchContains)
 
         self.ing1.setCompleter(completer)
@@ -143,71 +151,86 @@ class ingredients(QDialog, QWidget):
 
         submit = QPushButton("Submit", self)
         submit.move(3 * (self.width // 4), self.height + 75)
+        submit.clicked.connect(self.submit)
 
         self.show()
 
     def add(self):
-        if self.ing9.isEnabled():
-            self.ing10.setDisabled(False)
-        if self.ing8.isEnabled():
-            self.ing9.setDisabled(False)
-        if self.ing7.isEnabled():
-            self.ing8.setDisabled(False)
-        if self.ing6.isEnabled():
-            self.ing7.setDisabled(False)
-        if self.ing5.isEnabled():
-            self.ing6.setDisabled(False)
-        if self.ing4.isEnabled():
-            self.ing5.setDisabled(False)
-        if self.ing3.isEnabled():
-            self.ing4.setDisabled(False)
-        if self.ing2.isEnabled():
-            self.ing3.setDisabled(False)
-        if self.ing1.isEnabled():
-            self.ing2.setDisabled(False)
+        for x in range(len(self.ing) - 1, 0, -1):
+            if self.ing[x - 1].isEnabled():
+                self.ing[x].setDisabled(False)
 
     def remove(self):
-        if self.ing10.isEnabled():
-            self.ing10.setDisabled(True)
-            self.ing10.clear()
-        elif self.ing9.isEnabled():
-            self.ing9.setDisabled(True)
-            self.ing9.clear()
-        elif self.ing8.isEnabled():
-            self.ing8.setDisabled(True)
-            self.ing8.clear()
-        elif self.ing7.isEnabled():
-            self.ing7.setDisabled(True)
-            self.ing7.clear()
-        elif self.ing6.isEnabled():
-            self.ing6.setDisabled(True)
-            self.ing6.clear()
-        elif self.ing5.isEnabled():
-            self.ing5.setDisabled(True)
-            self.ing5.clear()
-        elif self.ing4.isEnabled():
-            self.ing4.setDisabled(True)
-            self.ing4.clear()
-        elif self.ing3.isEnabled():
-            self.ing3.setDisabled(True)
-            self.ing3.clear()
-        elif self.ing2.isEnabled():
-            self.ing2.setDisabled(True)
-            self.ing2.clear()
+        for x in self.ing[::-1][:-1]:
+            if x.isEnabled():
+                x.setDisabled(True)
+                x.clear()
+                return
+        # if self.ing10.isEnabled():
+        #     self.ing10.setDisabled(True)
+        #     self.ing10.clear()
+        # elif self.ing9.isEnabled():
+        #     self.ing9.setDisabled(True)
+        #     self.ing9.clear()
+        # elif self.ing8.isEnabled():
+        #     self.ing8.setDisabled(True)
+        #     self.ing8.clear()
+        # elif self.ing7.isEnabled():
+        #     self.ing7.setDisabled(True)
+        #     self.ing7.clear()
+        # elif self.ing6.isEnabled():
+        #     self.ing6.setDisabled(True)
+        #     self.ing6.clear()
+        # elif self.ing5.isEnabled():
+        #     self.ing5.setDisabled(True)
+        #     self.ing5.clear()
+        # elif self.ing4.isEnabled():
+        #     self.ing4.setDisabled(True)
+        #     self.ing4.clear()
+        # elif self.ing3.isEnabled():
+        #     self.ing3.setDisabled(True)
+        #     self.ing3.clear()
+        # elif self.ing2.isEnabled():
+        #     self.ing2.setDisabled(True)
+        #     self.ing2.clear()
 
     def clear(self):
-        self.ing1.clear()
-        self.ing2.clear()
-        self.ing3.clear()
-        self.ing4.clear()
-        self.ing5.clear()
-        self.ing6.clear()
-        self.ing7.clear()
-        self.ing8.clear()
-        self.ing9.clear()
-        self.ing10.clear()
+        for x in self.ing:
+            x.clear()
+            if x != self.ing1:
+                x.setDisabled(True)
         self.time.clear()
 
+    def submit(self):
+        if any([x.isEnabled() and x.text() == '' for x in self.ing]):
+            # self.submit.setStyleSheet("border :2px solid ;"
+            #                      "border-top-color : red; "
+            #                      "border-left-color : red;"
+            #                      "border-right-color : red;"
+            #                      "border-bottom-color : red")
+            warning = QMessageBox()
+            warning.setWindowTitle("Error")
+            warning.setText('Did not fill all the needed information')
+            warning.setIcon(QMessageBox.Critical)
+            x = warning.exec_()
+        elif not all([x.text() in self.clean for x in self.ing if x.isEnabled()]):
+            invalid = QMessageBox()
+            invalid.setWindowTitle("Error")
+            invalid.setText('One or more of the ingredients are invalid')
+            invalid.setIcon(QMessageBox.Critical)
+            x = invalid.exec_()
+        else:
+            print(type(self.time.text()))
+            if self.time.text() == '0':
+                next_page = QMessageBox()
+                next_page.setWindowTitle("Next")
+                next_page.setText('You did not set your time, would you still like to submit?')
+                next_page.setIcon(QMessageBox.Question)
+                # next_page.setStandardButtons(QMessageBox.Cancel | QMessageBox.Yes)
+                x = next_page.exec_()
+
+    # def third_page(self, i):
+    #     if i == "Yes":
 
 
     def center(self):  # Used top center the window on the desktop
